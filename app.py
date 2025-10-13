4b7dc21aac46962bb9e793f9eac54a0b89b64b3c
KonradBaba <babakonrad2@gmail.com>
2025-10-13 07:47:24 +0000

    Trigger webhook

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Cześć! Działam w kontenerze Docker!</h1>'
+    return '<h1>Cześć! Webhook DZIA</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
